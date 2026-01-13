a6073b5b7d83895f326b4dd2880f2109b9a937bc
nishankhanal12 <nishankhanal507@gmail.com>
2026-01-13 16:06:24 +0000

    number and math functions

---
+++ 01.basics/02_num_and_math.js
@@ -0,0 +1,14 @@
+const score = 9
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString());
+
+console.log(Math);
+console.log(Math.abs(-4)); // absolute value which is always positive
