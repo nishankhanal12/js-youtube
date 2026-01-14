c2d0f882a39690932da70d017534b548faa17b1d
nishankhanal12 <nishankhanal507@gmail.com>
2026-01-14 15:35:11 +0000

    objects destructure and json intro

---
+++ 01.basics/04_objects.js
@@ -0,0 +1,12 @@
+// Object de-structure and JSON API into
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+// instead of always mentioning course.courseInstructor always you can use
+//destructing object
+const{courseInstructor : instructor} = course;
+// console.log(courseInstructor);
+console.log(instructor);
